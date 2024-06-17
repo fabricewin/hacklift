ab7d11e3e79cdfe4a95de9227da64a5b4428d75b
Fabrice <azonhinfab@gmail.com>
2024-06-17 12:50:34 +0100

    Add variables and palindrome

---
+++ variable.ts
@@ -0,0 +1,10 @@
+console.log("..............LET.............")
+let myName2 = "Fabrice";
+myName2 = "Obed";
+console.log(myName2)
+
+console.log(".............CONST.........")
+const myName = "Fabrice";
+myName ="Obed";
+console.log(myName)
+
